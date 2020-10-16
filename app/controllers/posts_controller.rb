eafdd3538ef05e8112ec969ea4905faf008e0d8d
ryoichi1006 <kubota.r.10006@gmail.com>
2020-10-16 19:34:44 +0900

    GOOD RANKINGにGOOD数の多い３つの投稿を表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.limit(10).order('created_at DESC')
     @search_posts = Post.all.page(params[:page]).search(params[:search])
+    @all_ranks = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :image, :title, :act, :words, :reason, :category_id,  presence: true
 
+  has_many :likes
   belongs_to :user, foreign_key: 'user_id'
   belongs_to :category, optional: true
   mount_uploader :image, ImageUploader
@@ -9,7 +10,7 @@ class Post < ApplicationRecord
     if search
       where(['title LIKE ?', "%#{search}%"])
     else
-      
+
     end
   end
 
